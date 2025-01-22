6ed1faf3c8bb9dc282eb364491de1fb4146e0e79
Sabeel <sabeel.sabek@gmail.com>
2025-01-22 23:50:38 +0530

    add requirements.txt

--- manage.py
+++ manage.py
@@ -6,6 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_ALLOWED_HOSTS', 'etsygenserver.onrender.com,localhost,127.0.0.1')
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ChromeExtensionWithDjango.settings')
     try:
         from django.core.management import execute_from_command_line
